e552ba5381f10aa875f15602a155d19e720556c5
Carter Snook <cartersnook04@gmail.com>
2020-08-12 14:01:38 -0500

    Refactored code for cli use

--- minifier.ts
+++ minifier.ts
@@ -12,23 +12,36 @@ export class Minifier {
   decoder = new TextDecoder("utf-8")
 
   /**
-   * Checks to see if a Mime Type is supported
-   * @param mimeType The string that is the Mime Type that is being checked to see if it is currently supported.
+   * Gets the language of a file based upon the filepath.
+   * @param filepath The string that is the filepath of the file that will be scanned for the file extension.
    */
-  isAllowedMimeType = (mimeType: string): boolean =>
-    Object.prototype.hasOwnProperty.call(minifiers, mimeType)
+  getLang = (filepath: string): string => {
+    const re = /(?:\.([^.]+))?$/
+    const extension = re.exec(filepath)
+
+    if (extension && extension.length > 1) return extension[1]
+
+    throw Error(`Invalid path for file: ${filepath}`)
+  }
+
+  /**
+   * Checks to see if a language is supported.
+   * @param lang The string that is the language that is being checked to see if it is currently supported.
+   */
+  isAllowedLang = (lang: string): boolean =>
+    Object.prototype.hasOwnProperty.call(minifiers, lang)
 
   /**
    * Minifies and returns a string based upon a given mime type.
    * @param str The string that is the text that will be minified.
-   * @param mimeType The string that is the Mime Type that is being checked to see if it is currently supported.
+   * @param lang The string that is the language that is being checked to see if it is currently supported.
    */
-  string = (str: string, mimeType: string): string => {
-    if (!this.isAllowedMimeType(mimeType)) {
-      throw Error(`Mime Type ${mimeType} not allowed`)
+  string = (str: string, lang: string): string => {
+    if (!this.isAllowedLang(lang)) {
+      throw Error(`${lang} not allowed`)
     }
 
-    const minifier = minifiers[mimeType]
+    const minifier = minifiers[lang]
 
     return minifier(str)
   }
@@ -37,17 +50,19 @@ export class Minifier {
    * Writes a given file with a minified version of the original contents using a given mime type.
    * Requires the `--allow-read` and `--allow-write` flags.
    * @param filepath The string that is the filepath of the file that will be minified.
-   * @param mimeType The string that is the Mime Type that is being checked to see if it is currently supported.
+   * @param lang The unrequired string that is the language that is being checked to see if it is currently supported.
    * @async
    */
-  file = async (filepath: string, mimeType: string): Promise<void> => {
-    if (!this.isAllowedMimeType(mimeType)) {
-      throw Error(`Mime Type ${mimeType} not allowed`)
+  file = async (filepath: string, lang?: string): Promise<void> => {
+    if (lang === undefined) lang = this.getLang(filepath)
+
+    if (!this.isAllowedLang(lang)) {
+      throw Error(`${lang} not allowed`)
     }
 
     const file = this.decoder.decode(await Deno.readFile(filepath))
 
-    const minifier = minifiers[mimeType]
+    const minifier = minifiers[lang]
 
     await Deno.writeFile(filepath, this.encoder.encode(minifier(file)))
   }
