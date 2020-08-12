a797483820c100a091af45535f4d2ab2e87d75af
Carter Snook <cartersnook04@gmail.com>
2020-08-12 14:01:46 -0500

    Refactored code for CLI use

--- minifiers.ts
+++ minifiers.ts
@@ -1,5 +1,5 @@
 export const minifiers: { [key: string]: any } = {
-  "text/css": (css: string): string =>
+  css: (css: string): string =>
     css
       .replace(/\/\*.*\*\/|\/\*[\s\S]*?\*\/|\n|\t|\v|\s{2,}/g, "")
       .replace(/\s*\{\s*/g, "{")
@@ -13,10 +13,9 @@ export const minifiers: { [key: string]: any } = {
       .replace(/\s*\!\s*/g, " !")
       .replace(/\s*\;\}\s*/g, "}"),
 
-  "text/html": (html: string): string =>
+  html: (html: string): string =>
     html.replace(/\s\s+/g, "").replace(/\n/g, "").trim(),
 
   // TODO: Better regex to remove whitespace for arrays and objects
-  "application/json": (json: string): string =>
-    json.replace(/\n/g, "").replace(/\s+"/g, '"'),
+  json: (json: string): string => json.replace(/\n/g, "").replace(/\s+"/g, '"'),
 }
